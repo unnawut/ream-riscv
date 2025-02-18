e487d539ba9a7caf2a98fe2ad2fa0a1e3e669fcb
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2025-02-18 10:34:04 -0700

    fix: compute_shuffled_index computing wrong index (#109)
    * fix: compute_shuffled_index computing wrong index
    
    * fix: address PR feedback

--- crates/common/consensus/src/misc.rs
+++ crates/common/consensus/src/misc.rs
@@ -22,20 +22,22 @@ pub fn compute_signing_root<SSZObject: TreeHash>(ssz_object: SSZObject, domain: 
 }
 
 pub fn compute_shuffled_index(
-    index: usize,
+    mut index: usize,
     index_count: usize,
     seed: B256,
 ) -> anyhow::Result<usize> {
     ensure!(index < index_count, "Index must be less than index_count");
-    let mut index = index;
     for round in 0..SHUFFLE_ROUND_COUNT {
         let seed_with_round = [seed.as_slice(), &round.to_le_bytes()].concat();
         let pivot = bytes_to_int64(&hash(&seed_with_round)[..]) % index_count as u64;
 
         let flip = (pivot as usize + (index_count - index)) % index_count;
         let position = max(index, flip);
-        let seed_with_position =
-            [seed_with_round.as_slice(), &(position / 256).to_le_bytes()].concat();
+        let seed_with_position = [
+            seed_with_round.as_slice(),
+            &(position / 256).to_le_bytes()[0..4],
+        ]
+        .concat();
         let source = hash(&seed_with_position);
         let byte = source[(position % 256) / 8];
         let bit = (byte >> (position % 8)) % 2;
